0c896afee817d0a919408235046c9e6a0da43f38
周必稳 <564648237@qq.com>
2017-05-25 11:42:58 +0800

    已有项目换远程仓库

---
+++ TestRespository/TestRespositoryDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestRespositoryDemo
+//
+//  Created by 周必稳 on 2017/5/25.
+//  Copyright © 2017年 iocdoc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("123123123")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
